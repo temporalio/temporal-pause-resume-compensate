32b2ac1b8c8ef1a72a27752bf07772a8e499370d
Tihomir Surdilovic <tihomir@temporal.io>
2024-03-29 12:57:30 -0400

    first init
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

---
+++ src/main/java/io/temporal/sample/SampleIntApplication.java
@@ -0,0 +1,11 @@
+package io.temporal.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleIntApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleIntApplication.class, args).start();;
+    }
+}
