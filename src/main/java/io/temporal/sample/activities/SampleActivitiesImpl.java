d8de5b7d6f6612bf6913ba941e41c58d9560897d
Tihomir Surdilovic <tihomir@temporal.io>
2024-03-29 14:14:44 -0400

    update
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- src/main/java/io/temporal/sample/activities/SampleActivitiesImpl.java
+++ src/main/java/io/temporal/sample/activities/SampleActivitiesImpl.java
@@ -3,26 +3,43 @@ package io.temporal.sample.activities;
 import io.temporal.spring.boot.ActivityImpl;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 @Component
 @ActivityImpl(taskQueues = "samplequeue")
 public class SampleActivitiesImpl implements SampleActivities {
     @Override
     public String one() {
-        return null;
+        randSleep();
+        return "done...";
     }
 
     @Override
     public String two() {
-        return null;
+        randSleep();
+        return "done...";
     }
 
     @Override
     public String three() {
-        return null;
+        randSleep();
+        return "done...";
     }
 
     @Override
     public String four() {
-        return null;
+        randSleep();
+        return "done...";
+    }
+
+    private void randSleep() {
+        int min = 1000;
+        int max = 4000;
+        try {
+            TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(min, max));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/io/temporal/sample/workflows/SampleWorkflowImpl.java
+++ src/main/java/io/temporal/sample/workflows/SampleWorkflowImpl.java
@@ -1,12 +1,47 @@
 package io.temporal.sample.workflows;
 
+import io.temporal.activity.ActivityOptions;
+import io.temporal.sample.activities.SampleActivities;
 import io.temporal.sample.model.SampleInput;
 import io.temporal.spring.boot.WorkflowImpl;
+import io.temporal.workflow.Async;
+import io.temporal.workflow.CancellationScope;
+import io.temporal.workflow.Promise;
+import io.temporal.workflow.Workflow;
+import org.slf4j.Logger;
+
+import java.time.Duration;
 
 @WorkflowImpl(taskQueues = "samplequeue")
 public class SampleWorkflowImpl implements SampleWorkflow {
+    private Logger logger = Workflow.getLogger(SampleWorkflowImpl.class);
+    private SampleActivities activities = Workflow.newActivityStub(SampleActivities.class,
+            ActivityOptions.newBuilder()
+                    .setStartToCloseTimeout(Duration.ofSeconds(4))
+                    .build());
+
     @Override
     public String run(SampleInput input) {
-        return "{\"result\":\"done..." + input.getTimer() +"\"}";
+
+        // Create the timer promise
+        Promise<Void> timerPromise = Workflow.newTimer(Duration.ofSeconds(input.getTimer()));
+        // Create activities promise
+        Promise<Void> activitiesPromise = Async.procedure(this::runActivities);
+
+        Promise.anyOf(timerPromise, activitiesPromise).get();
+        if(timerPromise.isCompleted()) {
+            // need to cancel activities here and compensate...
+            return "{\"result\":\"timer completed first..." + input.getTimer() +"\"}";
+        } else {
+            return "{\"result\":\"activities completed first..." + input.getTimer() +"\"}";
+        }
     }
+
+    private void runActivities() {
+        activities.one();
+        activities.two();
+        activities.three();
+        activities.four();
+    }
+
 }
