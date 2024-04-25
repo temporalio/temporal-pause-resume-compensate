97abfd56a3a42c396c86494fa9a279152314ee6a
Keith Tenzer <ktenzer@keiths-mbp.lan>
2024-04-25 15:51:17 -0700

    added random fail logic to activities
    Signed-off-by: Keith Tenzer <ktenzer@keiths-mbp.lan>

--- src/main/java/io/temporal/sample/activities/SampleActivitiesImpl.java
+++ src/main/java/io/temporal/sample/activities/SampleActivitiesImpl.java
@@ -7,6 +7,8 @@ import io.temporal.failure.ApplicationFailure;
 import io.temporal.sample.model.SampleResult;
 import io.temporal.spring.boot.ActivityImpl;
 import org.springframework.stereotype.Component;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,21 +18,22 @@ public class SampleActivitiesImpl implements SampleActivities {
     @Override
     public SampleResult one() {
         sleep(1);
+        thisMayOrMayNotThrowAnError("activity four");
         return new SampleResult("Activity one done...");
     }
 
     @Override
     public SampleResult two() {
         sleep(1);
+        thisMayOrMayNotThrowAnError("activity four");
         return new SampleResult("Activity two done...");
     }
 
     @Override
     public SampleResult three() {
         sleep(1);
-//        return new SampleResult("Activity three done...");
-        throw ApplicationFailure.newNonRetryableFailure("simulated activity failure from three",
-                "some error", null);
+        thisMayOrMayNotThrowAnError("activity four");
+        return new SampleResult("Activity three done...");
     }
 
     @Override
@@ -46,6 +49,7 @@ public class SampleActivitiesImpl implements SampleActivities {
 //            }
 //        }
         sleep(1);
+        thisMayOrMayNotThrowAnError("activity four");
         return new SampleResult("Activity four done...");
     }
 
@@ -54,6 +58,15 @@ public class SampleActivitiesImpl implements SampleActivities {
             Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
         } catch (InterruptedException ee) {
             // Empty
+        }
+    }
+
+    private void thisMayOrMayNotThrowAnError(String activityName) {
+        Random random = new Random();
+        double randomValue = random.nextDouble();
+        if (randomValue < 0.10) { // 10% chance of failure
+            throw ApplicationFailure.newNonRetryableFailure("simulated failure from " + activityName,
+                    "some error", null);
         }
     }
 }
